be3003818c699cad6284d61e625fea2ea980e07b
walnuts1018 <r.juglans.1018@gmail.com>
2026-05-31 00:47:39 +0900

    refactor: remove app_runtime functionality and related tests
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- main/app_runtime.c
+++
@@ -1,6 +0,0 @@
-#include "app_runtime.h"
-
-bool app_runtime_should_start_api(const app_config_t *config, bool wifi_provisioning)
-{
-    return app_config_has_wifi(config) && !wifi_provisioning;
-}
--- main/main.c
+++ main/main.c
@@ -1,6 +1,5 @@
 #include "api_server.h"
 #include "app_config.h"
-#include "app_runtime.h"
 #include "auth_oidc.h"
 #include "esp_log.h"
 #include "sensor_task.h"
@@ -19,7 +18,7 @@ void app_main(void)
              app_config_has_auth_audience(&config) ? "set" : "missing");
 
     ESP_ERROR_CHECK(wifi_manager_start(&config));
-    if (!app_runtime_should_start_api(&config, wifi_manager_is_provisioning())) {
+    if (!app_config_has_wifi(&config) || wifi_manager_is_provisioning()) {
         ESP_LOGI(TAG, "Wi-Fi provisioning is active; API server will start after provisioning reboot");
         return;
     }
